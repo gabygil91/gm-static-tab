ff18a4d9d48547be344a4c55ab5f9581625ceb65
Gabriela Gil <gabrielagil247@gmail.com>
2017-10-25 17:39:08 -0500

    validation

--- app/js/controllers/View1Controller.js
+++ app/js/controllers/View1Controller.js
@@ -10,16 +10,23 @@ myApp.controller('view1Controller', function($scope, $location, $http) {
     })
 
     microsoftTeams.initialize();
-    microsoftTeams.settings.registerOnSaveHandler(function(saveEvent){
-        microsoftTeams.settings.setSettings({
-             entityId: "gmQA",
-             contentUrl: "https://stark-chamber-28124.herokuapp.com/main",
-             suggestedDisplayName: "GM Q/A",
-             websiteUrl: "https://stark-chamber-28124.herokuapp.com/main",
-             removeUrl: "https://teams-get-started-sample.azurewebsites.net/tabremove.html",
-          });
-        saveEvent.notifySuccess();
-    });
+    microsoftTeams.settings.setSettings({
+        entityId: "gmQA",
+        contentUrl: "https://stark-chamber-28124.herokuapp.com/main",
+        suggestedDisplayName: "GM Q/A",
+        websiteUrl: "https://stark-chamber-28124.herokuapp.com/main",
+        removeUrl: "https://teams-get-started-sample.azurewebsites.net/tabremove.html",
+     });
+    // microsoftTeams.settings.registerOnSaveHandler(function(saveEvent){
+    //     microsoftTeams.settings.setSettings({
+    //          entityId: "gmQA",
+    //          contentUrl: "https://stark-chamber-28124.herokuapp.com/main",
+    //          suggestedDisplayName: "GM Q/A",
+    //          websiteUrl: "https://stark-chamber-28124.herokuapp.com/main",
+    //          removeUrl: "https://teams-get-started-sample.azurewebsites.net/tabremove.html",
+    //       });
+    //    // saveEvent.notifySuccess();
+    // });
 
     function onClick(){
         microsoftTeams.settings.setValidityState(true);
