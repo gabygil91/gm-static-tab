1d3a7b89513fb29dbba3de86dfac161872615db7
Gabriela Gil <gabrielagil247@gmail.com>
2017-10-25 17:34:21 -0500

    validation

--- app/js/controllers/View1Controller.js
+++ app/js/controllers/View1Controller.js
@@ -1,6 +1,27 @@
-'use strict';
+'use strict'
 
-myApp.controller('View1Controller',
-    function ProjectsController($scope, $location) {
-        console.log("view 1 controller");
-});
+myApp.controller('view1Controller', function($scope, $location, $http) {
+    $scope.title = "this is the title whooo";
+    $scope.onClick = onClick;
+  $http.get("https://jsonplaceholder.typicode.com/posts/1")
+    .then(function(response) {
+      console.log(response.data)
+      $scope.myWelcome = response.data
+    })
+
+    microsoftTeams.initialize();
+    microsoftTeams.settings.registerOnSaveHandler(function(saveEvent){
+        microsoftTeams.settings.setSettings({
+             entityId: "gmQA",
+             contentUrl: "https://stark-chamber-28124.herokuapp.com/main",
+             suggestedDisplayName: "GM Q/A",
+             websiteUrl: "https://stark-chamber-28124.herokuapp.com/main",
+             removeUrl: "https://teams-get-started-sample.azurewebsites.net/tabremove.html",
+          });
+        saveEvent.notifySuccess();
+    });
+
+    function onClick(){
+        microsoftTeams.settings.setValidityState(true);
+    }
+})
