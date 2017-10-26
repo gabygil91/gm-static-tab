5c1ea16987daebe870f7f9b7b05e1fdd22b73963
Gabriela Gil <gabrielagil247@gmail.com>
2017-10-26 11:23:33 -0500

    validation

--- app/js/controllers/MainController.js
+++ app/js/controllers/MainController.js
@@ -1,7 +1,17 @@
-'use strict'
-
-myApp.controller('mainController', function($scope) {
-    $scope.title = "this is the title whooo";
-    $scope.test = "test"
-  
+myApp.controller('mainController', function($scope, $location, $http) {
+    $scope.title = "Unanswered Questions";
+    $scope.masonryConfig = {
+        'transitionDuration': '0.7s',
+        'gutter': 5,
+        'itemSelector': '.item',
+        'isOriginTop': true,
+        'isOriginLeft': true,
+        'columnWidth': 10,
+        'gridStyle': ''
+    }
+    $http.get("https://hackatron-api.herokuapp.com/api/getAllQuestions")
+    .then(function(response) {
+      $scope.results = response.data;
+    })
+ 
 })
--- app/js/controllers/View1Controller.js
+++ app/js/controllers/View1Controller.js
@@ -2,21 +2,5 @@
 
 myApp.controller('view1Controller', function($scope, $location, $http) {
     $scope.title = "this is the title whooo";
-    //$scope.onClick = onClick;
-  $http.get("https://jsonplaceholder.typicode.com/posts/1")
-    .then(function(response) {
-      console.log(response.data)
-      $scope.myWelcome = response.data
-    })
-
-    // microsoftTeams.initialize();
-    // microsoftTeams.settings.setSettings({
-    //     entityId: "gmQA",
-    //     contentUrl: "https://stark-chamber-28124.herokuapp.com",
-    //     suggestedDisplayName: "GM Q/A",
-    //     websiteUrl: "https://stark-chamber-28124.herokuapp.com"
-    //  });
-    // function onClick(){
-    //     microsoftTeams.settings.setValidityState(true);
-    // }
+ 
 })
