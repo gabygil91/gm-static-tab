e910af4aca978244cd3c9c50e22677a1768c5223
Gabriela Gil <gabrielagil247@gmail.com>
2017-10-26 08:54:31 -0500

    validation

--- app/js/controllers/MainController.js
+++ app/js/controllers/MainController.js
@@ -3,14 +3,5 @@
 myApp.controller('mainController', function($scope) {
     $scope.title = "this is the title whooo";
     $scope.test = "test"
-    microsoftTeams.initialize();
-    microsoftTeams.settings.setSettings({
-        entityId: "gmQA",
-        contentUrl: "https://stark-chamber-28124.herokuapp.com",
-        suggestedDisplayName: "GM Q/A",
-        websiteUrl: "https://stark-chamber-28124.herokuapp.com"
-     });
-    //function onClick(){
-    microsoftTeams.settings.setValidityState(true);
-    //}
+  
 })
--- app/js/controllers/View2Controller.js
+++ app/js/controllers/View2Controller.js
@@ -1,6 +1,17 @@
-'use strict';
+'use strict'
 
-myApp.controller('View2Controller',
-    function SecretsController($scope, $location, secretsData) {
-
-});
+myApp.controller('view2Controller', function($scope) {
+    $scope.title = "View 2";
+    $scope.onClick = onClick;
+    
+    microsoftTeams.initialize();
+    microsoftTeams.settings.setSettings({
+        entityId: "gmQA",
+        contentUrl: "https://stark-chamber-28124.herokuapp.com",
+        suggestedDisplayName: "GM Q/A",
+        websiteUrl: "https://stark-chamber-28124.herokuapp.com"
+     });
+    function onClick(){
+        microsoftTeams.settings.setValidityState(true);
+    }
+})
